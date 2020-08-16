c6886a33b5a2c8300cd4cdb9d9181fb3073b088c
herculanoleo <herculanoleo@gmail.com>
2020-08-16 19:43:55 -0300

    SECOND COMMIT

--- livraria_api_rest/src/main/java/br/com/herculano/livararia_api_rest/LivrariaAPIRestApplication.java
+++ livraria_api_rest/src/main/java/br/com/herculano/livararia_api_rest/LivrariaAPIRestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LivrariaRestApplication {
+public class LivrariaAPIRestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LivrariaRestApplication.class, args);
+		SpringApplication.run(LivrariaAPIRestApplication.class, args);
 	}
 
 }
